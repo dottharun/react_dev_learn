34ee58d74617839fe380c13ccdc6b57a0600f030
ubutharun <tharun242424@gmail.com>
2023-10-28 15:44:36 +0530

    inital setup with vite react ts tailwindcss

---
+++ src/App.tsx
@@ -0,0 +1,11 @@
+export default function Profile() {
+  return (
+    <>
+      <img
+        className="border-8"
+        src="https://i.imgur.com/MK3eW3Am.jpg"
+        alt="Katherine Johnson"
+      />
+    </>
+  );
+}
