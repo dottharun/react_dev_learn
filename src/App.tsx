4aff77047d8d4076c28f8ec8cc0aac9770b101a5
ubutharun <tharun242424@gmail.com>
2023-10-29 15:31:31 +0530

    markup to jsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,13 @@
 import Gallery from "./components/01FirstComp/Gallery";
 import Profile from "./components/01FirstComp/Profile";
+import TodoList from "./components/02markup/TodoList";
 
 const App = () => {
   return (
     <>
       <Gallery />
       <Profile />
+      <TodoList />
     </>
   );
 };
